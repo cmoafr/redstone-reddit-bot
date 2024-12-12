07309bd65737a4a0f5186cb1434d7d1731115b8a
cmoafr <cmoa58@gmail.com>
2024-12-12 22:55:26 +0100

    Add automatic QC confirmation comment
    Send a comment on the post with:
    - Predefiend text
    - A copy of the "0 days since last QC" image
    - Pin the post (make sticky)

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,15 @@
 // Visit developers.reddit.com/docs to learn Devvit!
 
-import { Devvit } from '@devvit/public-api';
+import { Devvit, RichTextBuilder } from '@devvit/public-api';
+
+const qc0DaysUrl = 'https://i.redd.it/h97he7ckb5yd1.jpeg';
+
+const qcDescription = `
+<insert good QC description here>
+`;
 
 Devvit.configure({
+  media: true,
   redis: true,
   redditAPI: true,
 });
@@ -33,7 +40,26 @@ Devvit.addMenuItem({
 
     // Post has been confirmed as QC
     context.ui.showToast('QC post confirmed!');
-    // TODO: Send message (explaination, meme image, infos) and pin it
+
+    // Reuploading as each media ID can only be reused on the same post
+    const response = await context.media.upload({
+      url: qc0DaysUrl,
+      type: 'image',
+    });
+    const comment = await context.reddit.submitComment({
+      id: event.targetId,
+      // FIXME: The text field is still required for some reason
+      // Will check with the community and update as necessary
+      text: 'QC post confirmation',
+      richtext: new RichTextBuilder()
+        .paragraph((p) => {
+          // TODO: Exact content of the message has yet to be defined
+          p.text({text: qcDescription});
+        })
+        .image({ mediaId: response.mediaId }),
+    });
+
+    await comment.distinguish(true);
   },
 });
 
